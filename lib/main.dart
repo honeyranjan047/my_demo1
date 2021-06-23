73a4947131817591d63c8a6bc7497bd98b456d7f
Honeyranjan047 <honeyranjan9@gmail.com>
2021-06-23 16:03:36 +0530

    Code Reorganisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-          title: "My Flutter App",
-          home: Scaffold(
-            appBar: AppBar(title: Text("My First Android Flutter App"),),
-            body: Material(
-              color: Colors.blueAccent,
-              child: Center(
-                child: Text(
-                  ("Hello flutter"),
-                  textDirection: TextDirection.ltr,
-                ),
+  runApp( new FlutterClass()
+  );
+}
+
+class FlutterClass extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My Flutter App",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First Android Flutter App"),
+          ),
+          body: Material(
+            color: Colors.blueAccent,
+            child: Center(
+              child: Text(
+                ("Hello flutter"),
+                textDirection: TextDirection.ltr,
+                style: TextStyle (color: Colors.white),
               ),
             ),
-          )
-      )
-  );
+          ),
+        )
+    );
+    throw UnimplementedError();
+  }
+
 }
